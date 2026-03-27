e30c9d2472592efafc9ace8c1d28705b0587ec2b
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-03-27 19:52:11 +0530

    Time: 1 ms (99.47%), Space: 46.8 MB (82.4%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int row = mat.length;
+        int col = mat[0].length;
+        k %= col;
+        for(int i = 0; i<row;i++){
+            for(int j = 0; j < col;j++){
+                if(mat[i][j] != mat[i][(j+k)%col]) return false;
+            }
+        }
+        return true;
+    }
+}
