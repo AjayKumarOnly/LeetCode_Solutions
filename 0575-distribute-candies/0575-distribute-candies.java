3940e26e9203b447c9d70a0ddbf1916f9f8147f7
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-04-02 22:10:49 +0530

    Time: 30 ms (93.25%), Space: 48.3 MB (88.48%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -4,7 +4,7 @@ class Solution {
         for(int n : candyType){
             st.add(n);
         }
-        if(st.size() > candyType.length/2){
+        if(st.size() >= candyType.length/2){
             return candyType.length/2;
         }
         return st.size();
