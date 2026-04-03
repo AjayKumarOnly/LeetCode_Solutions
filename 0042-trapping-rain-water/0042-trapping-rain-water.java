13e7160acb5204679bdfe0db69e714b9f279482e
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-04-03 10:28:45 +0530

    Time: 1 ms (59.1%), Space: 47.8 MB (63.31%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int result = 0 , maxLeft = 0 , maxRight = 0 , left = 0, right = n -1;
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= maxLeft) maxLeft = height[left];
+                else result += maxLeft - height[left];
+                left++;
+            }
+            else {
+                if(height[right] >= maxRight) maxRight = height[right];
+                else result += maxRight - height[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
