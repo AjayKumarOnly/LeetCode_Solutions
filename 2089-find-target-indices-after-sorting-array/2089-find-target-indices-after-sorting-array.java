5db841821348d455e05ed0ea8d7a8bb6f7a3e034
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-04-14 10:07:37 +0530

    Time: 5 ms (73.9%), Space: 46.5 MB (39.41%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -2,11 +2,24 @@ class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         Arrays.sort(nums);
         List<Integer> res = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                res.add(i);
-            }
+        int start = binarySearch(nums,target);
+        int end = binarySearch(nums,target+1);
+
+        for (int i = start; i < end; i++) {
+            res.add(i);
         }
+
         return res;
+
+    }
+    private static int binarySearch(int[] nums,int target){
+        int left = 0, right = nums.length;
+        while(left < right){
+            int mid = (left+right)/2;
+            if(nums[mid] < target){
+                left = mid +1;
+            }else right = mid;
+        }
+        return left;
     }
 }
