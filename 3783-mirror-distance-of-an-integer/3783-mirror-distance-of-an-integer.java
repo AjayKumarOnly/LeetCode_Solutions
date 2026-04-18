ac93b71b2134aaa7f060beb150e27b9b4f2104eb
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-04-18 16:23:59 +0530

    Time: 1 ms (100%), Space: 42.6 MB (48.72%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
@@ -6,8 +6,8 @@ class Solution {
     private static int reverse(int x){
         int res = 0;
         while(x!=0){
-            int dig = x % 10;
-            res = res * 10 + dig;
+            //int dig = x % 10;
+            res = res * 10 + (x % 10);
             x /= 10;
         }
         return res;
