052b43e2b6210964ec071a4b20bddded7d2b909b
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-04-20 07:55:38 +0530

    Time: 0 ms (100%), Space: 43.2 MB (64.71%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -4,9 +4,5 @@ class Solution {
         while(colors[0] == colors[right]) right--;
         while(colors[n-1] == colors[left]) left++;
         return Math.max(right,n-1-left);
-        // int n = colors.length, i = 0, j = n - 1;
-        // while (colors[0] == colors[j]) j--;
-        // while (colors[n - 1] == colors[i]) i++;
-        // return Math.max(n - 1 - i, j);
     }
 }
