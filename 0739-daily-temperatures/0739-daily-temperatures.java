5942a5f50253057edf026a4f42b1e813ace2a59a
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-04-24 09:18:38 +0530

    Time: 79 ms (11.25%), Space: 106 MB (78.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -5,7 +5,7 @@ class Solution {
         int[] res = new int[n];
 
         for(int i= n-1;i>=0;i--){
-            while(!st.isEmpty() && temperatures[st.peek()] <= temperatures[i]){
+            while(!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]){
                 st.pop();
             }
             if(!st.isEmpty()) res[i] = st.peek()-i;
