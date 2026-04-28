814977443610e3af43794b51834e776d8164745c
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-04-28 22:02:14 +0530

    Time: 145 ms (12.21%), Space: 116 MB (20.61%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,24 +1,22 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
         List<Integer> temp = new ArrayList<>();
-        int n = grid.length;
-        int m = grid[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 temp.add(grid[i][j]);
             }
         }
-        Collections.sort(temp);
-        int low = 0 , high = temp.size() - 1;
-        int mid = (low + high) / 2 , target = temp.get(mid);
+         Collections.sort(temp);
+        int low = 0 , high = temp.size()-1;
+        int mid = (low + high) / 2;
+        int key = temp.get(mid);
 
-       int operations = 0;
-        for (int num : temp) {
-            if (Math.abs(num - target) % x != 0) {
-                return -1;
-            }
-            operations += Math.abs(num - target) / x;
+        int oper = 0;
+
+        for(int num : temp){
+            if(Math.abs(num - key) % x != 0) return -1;
+           oper += Math.abs(num-key) / x;
         }
-        return operations;
-    }
+        return oper;
+    } 
 }
