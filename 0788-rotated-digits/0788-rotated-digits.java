9291e9dd40185ebeba27c8879b9a156d42419226
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-05-02 06:59:11 +0530

    Time: 3 ms (91.63%), Space: 41.9 MB (82.58%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -0,0 +1,25 @@
+class Solution {
+    private static boolean isValid(int x){
+        boolean status = false;
+        while(x > 0){
+            int digit = x % 10;
+            if(digit == 3 || digit == 4 || digit == 7) {
+                return  false;
+            }
+            if(digit == 2 || digit == 5 || digit == 6 || digit == 9){
+                status = true;
+            }
+            x/=10;
+        }
+        return status;
+    }
+    public int rotatedDigits(int n) {
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(isValid(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
