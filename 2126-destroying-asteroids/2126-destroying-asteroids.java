566058aae0861f0b9c65975e640e8addd24ff859
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-05-31 07:35:40 +0530

    Time: 26 ms (81.52%), Space: 107.6 MB (16.4%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         int n = asteroids.length;
-        long  mas = (long)mass;
+        long mas = (long) mass;
         Arrays.sort(asteroids);
         for (int i = 0; i < n; i++) {
 
-            if (mas >= asteroids[i])
-                mas += asteroids[i];
-            else if (mas < asteroids[i])
-                mas -= asteroids[i];
+            if (mas < asteroids[i])
+                return false;
+            mas += asteroids[i];
         }
-        if (mas <= 0)
-            return false;
         return true;
     }
 }
