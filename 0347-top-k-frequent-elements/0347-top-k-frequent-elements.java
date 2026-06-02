fc337246ee58def650b7ed18335bee51418bba58
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-06-02 09:13:04 +0530

    Time: 13 ms (79.67%), Space: 47.6 MB (64.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -9,16 +9,21 @@ class Solution {
         for(int i : nums){
             mpp.put(i,mpp.getOrDefault(i,0)+1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>> minHeap = new PriorityQueue<>(new FreqComparator());
+        // O(N)
+        PriorityQueue<Map.Entry<Integer,Integer>> maxHeap = new PriorityQueue<>(new FreqComparator());
         int[] result = new int[k];
 
         for(Map.Entry<Integer,Integer> entry : mpp.entrySet()){
-            minHeap.offer(entry);
+            maxHeap.offer(entry); // log(N)
         }
+        // m times log(n);
+        // so O(m log n)
+        
 
         for(int i=0;i<k;i++){
-            result[i] = minHeap.poll().getKey();
+            result[i] = maxHeap.poll().getKey();
 }
         return result;
     }
 }
+// sp -> O(k)
