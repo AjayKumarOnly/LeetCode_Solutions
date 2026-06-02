d82e17c7f58a7d0056d570d34011b5084e72006b
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-06-02 07:50:13 +0530

    Time: 6 ms (99.59%), Space: 46.5 MB (66.86%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,28 @@
+class WordFreqComparator implements Comparator<Map.Entry<String,Integer>>{
+    public int compare(Map.Entry<String,Integer> a ,Map.Entry<String,Integer> b){
+        if(a.getValue().equals(b.getValue())){
+            return b.getKey().compareTo(a.getKey());
+        }
+        return a.getValue() - b.getValue(); // negative ->  no change ||  pos -> change || this is minHeap;
+    }
+}
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String,Integer> mpp = new HashMap<>();
+        for(String s : words){
+            mpp.put(s,mpp.getOrDefault(s,0)+1);
+        }
+        PriorityQueue<Map.Entry<String ,Integer>> minHeap = new PriorityQueue<>(new WordFreqComparator());
+        for(Map.Entry<String,Integer> x : mpp.entrySet()) {
+            minHeap.offer(x);
+            if(minHeap.size() > k){
+                minHeap.remove();
+            }
+        }
+        List<String> res = new ArrayList<>();
+        while(!minHeap.isEmpty()){
+            res.add(0,minHeap.poll().getKey());
+        }
+        return res;
+    }
+}
