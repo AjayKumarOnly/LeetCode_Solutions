e03d086a4b5d1f77dc24100d289b47e796a039f4
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-06-02 18:52:47 +0530

    Time: 5 ms (95.66%), Space: 45.6 MB (56.54%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -0,0 +1,36 @@
+class FreqComparator implements Comparator<Map.Entry<Integer,Integer>>{
+    public int compare(Map.Entry<Integer,Integer> a, Map.Entry<Integer,Integer> b){
+        if(a.getValue() == b.getValue()){
+            return b.getKey() - a.getKey();
+        }
+        return a.getValue() - b.getValue();
+    }
+}
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        PriorityQueue<Map.Entry<Integer,Integer>> minHeap = new PriorityQueue<>(new FreqComparator());
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        for(int i : nums){
+            mpp.put(i , mpp.getOrDefault(i,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry : mpp.entrySet()){
+            minHeap.offer(entry);
+        }
+        int[] result = new int[nums.length];
+        int index = 0;
+
+        while(!minHeap.isEmpty()) {
+
+            Map.Entry<Integer,Integer> entry = minHeap.poll();
+
+            int num = entry.getKey();
+            int freq = entry.getValue();
+
+            for(int i = 0; i < freq; i++) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+    }
+}
