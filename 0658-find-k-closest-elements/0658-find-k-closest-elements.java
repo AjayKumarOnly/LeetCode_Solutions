07f8fe2c512b34cd1bc94dcfda40b40a6bfdc0b6
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-06-02 22:05:07 +0530

    Time: 40 ms (19.68%), Space: 47.9 MB (33.83%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -0,0 +1,28 @@
+class ClosestComparator implements Comparator<Integer>{
+    int x = 0;
+    public ClosestComparator(int x){
+        this.x = x;
+    }
+    public int compare(Integer a , Integer b){
+        int diff = Math.abs(b-x) - Math.abs(a-x);
+        if(diff == 0){
+            return b - a;
+        }
+        return diff;
+    }
+}
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new ClosestComparator(x));
+        for(int i : arr){
+            pq.offer(i);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        List<Integer> res = new ArrayList<>(pq);
+        Collections.sort(res);
+        return res;
+    }
+}
