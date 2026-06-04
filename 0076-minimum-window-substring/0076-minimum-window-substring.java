e084c2a54165625b47263a95a509b616f95575c6
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-06-04 21:04:04 +0530

    Time: 46 ms (18.49%), Space: 45.6 MB (73.39%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(s.length() < t.length()) return "";
+
+        int[] tFreq = new int[128];
+        int[] windowFreq = new int[128];
+
+        for(char c : t.toCharArray()){
+            tFreq[c]++;
+        }
+        int left = 0 , minLen = Integer.MAX_VALUE , start = 0;
+        for(int right = 0; right < s.length(); right++){
+            windowFreq[s.charAt(right)]++;
+
+            while(containsAll(tFreq,windowFreq)){
+                int currLen = right - left + 1;
+                if(currLen < minLen){
+                    minLen = currLen;
+                    start = left;
+                }
+                windowFreq[s.charAt(left)]--;
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start,start + minLen);
+    }
+    private boolean containsAll(int[] tFreq , int[] windowFreq){
+        for(int i = 0; i < 128; i++){
+            if(windowFreq[i] < tFreq[i]) return false;
+        }
+        return  true;
+    }
+}
