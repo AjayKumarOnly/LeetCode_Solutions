bf37ec6e249d502dd69437c1688df3259576e3a7
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-06-16 11:00:51 +0530

    Time: 1 ms (99.91%), Space: 45.2 MB (79.49%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,19 +1,19 @@
 class Solution {
     public int compress(char[] chars) {
-        int index = 0 , cnt = 0;
+        int cnt = 0 , ind = 0;
         for(int i = 0; i < chars.length; i++){
             cnt++;
-            if(i+1 == chars.length || chars[i] != chars[i+1]){
-                chars[index++] = chars[i];
+            // i + 1 == chars.length [checks out of range]
+            if(i + 1 == chars.length || chars[i] != chars[i+1]){
+                chars[ind++] = chars[i];
                 if(cnt != 1){
-                    for(char c : Integer.toString(cnt).toCharArray()){
-                         chars[index++] = c;
+                    for(char c : Integer.toString(cnt).toCharArray()){ // ex cnt = 12 - > ['1','2']
+                             chars[ind++] = c;
                     }
                 }
-                 cnt = 0;
+                cnt = 0; // reset the cnt = 0
             }
-           
         }
-        return index;
+        return ind;
     }
 }
