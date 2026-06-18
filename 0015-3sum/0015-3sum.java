207a03a9ce6ae193af711327bb54be74af5d475a
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-06-18 09:18:51 +0530

    Time: 35 ms (43.35%), Space: 59 MB (84.85%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,22 +3,29 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         List<List<Integer>> res = new ArrayList<>();
-        for(int i = 0; i < n -1;i++){
+
+        for(int i = 0; i < n-1; i++){
             if(i==0 || i > 0 && nums[i]!=nums[i-1]){
-                int low = i+1, high = n -1,target = -nums[i];
-                while( low < high){
-                    int curr = nums[low] + nums[high];
-                    if(curr == target){
-                        res.add(Arrays.asList(nums[i],nums[low],nums[high]));
-                        while(low < high && nums[low] == nums[low+1]) low++;
-                        while(low < high && nums[high] == nums[high -1]) high--;
-                        low++;
-                        high--;
-                    }else if (curr > target) high--;
-                    else{
+            int low = i+1 , right = n-1 , target = -nums[i];
+
+            while(low < right){
+                int currSum = nums[low] + nums[right];
+
+                if(currSum == target){
+                    res.add(Arrays.asList(nums[i],nums[low],nums[right]));
+
+                    while(low < right && nums[low] == nums[low+1]) {
                         low++;
                     }
+                    while(low < right && nums[right] == nums[right-1]) {
+                      right--;
+                    }
+                    low++;
+                    right--;
                 }
+                else if (currSum > target) right--;
+                else low++;
+            }
             }
         }
         return res;
