7425547ab3587a80cff6b2c14a115c8d36eaf77d
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-06-22 07:58:28 +0530

    Time: 2 ms (96.67%), Space: 43.5 MB (54.89%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,31 +1,18 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
 
-        Map<Character, Integer> mpp = new HashMap<>();
+        int[] freq = new int[26];
 
         for(char c : text.toCharArray()){
-            mpp.put(c, mpp.getOrDefault(c, 0) + 1);
+            freq[c - 'a']++;
         }
 
-        int ans = 0;
-
-        while(
-            mpp.getOrDefault('b', 0) >= 1 &&
-            mpp.getOrDefault('a', 0) >= 1 &&
-            mpp.getOrDefault('l', 0) >= 2 &&
-            mpp.getOrDefault('o', 0) >= 2 &&
-            mpp.getOrDefault('n', 0) >= 1
-        ){
-
-            mpp.put('b', mpp.get('b') - 1);
-            mpp.put('a', mpp.get('a') - 1);
-            mpp.put('l', mpp.get('l') - 2);
-            mpp.put('o', mpp.get('o') - 2);
-            mpp.put('n', mpp.get('n') - 1);
-
-            ans++;
-        }
-
-        return ans;
+        return Math.min(
+            Math.min(freq['b' - 'a'], freq['a' - 'a']),
+            Math.min(
+                Math.min(freq['l' - 'a'] / 2, freq['o' - 'a'] / 2),
+                freq['n' - 'a']
+            )
+        );
     }
 }
