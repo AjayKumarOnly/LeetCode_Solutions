66f3f2129d84d4468e03b06e5d6bd700767cea59
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-07-04 10:28:07 +0530

    Time: 1 ms (97.89%), Space: 47.1 MB (55.83%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,20 +1,24 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new ArrayList<>();
+        List<int[]> res = new ArrayList<>();
 
         for(int[] interval : intervals){
+            // newInterval -> [1,2]   curr Interval -> [4,5]
             if(newInterval[1] < interval[0]){
-                result.add(newInterval);
+                res.add(newInterval);
                 newInterval = interval;
             }
+            // curr Interval -> [3,4]  newInterval -> [5,7]
             else if(interval[1] < newInterval[0]){
-                result.add(interval);
-            }else{
+                res.add(interval);
+            }
+            // this is overlapping condition 
+            else {
                 newInterval[0] = Math.min(newInterval[0],interval[0]);
                 newInterval[1] = Math.max(newInterval[1],interval[1]);
             }
         }
-        result.add(newInterval);
-        return result.toArray(new int[result.size()][]);
+        res.add(newInterval);
+        return res.toArray(new int[res.size()][]);
     }
 }
