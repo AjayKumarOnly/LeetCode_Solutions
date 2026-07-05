52ac3c47e121709c0f6669729dd2b25c22106094
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-07-05 21:07:24 +0530

    Time: 137 ms (52.34%), Space: 43.1 MB (43.65%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -6,15 +6,15 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == word.charAt(0)
-                        && dfs(i, j, 0, board, word)) {
+                if (board[i][j] == word.charAt(0) && dfs(i, j, 0, board, word)) {
                     return true;
                 }
             }
         }
         return false;
     }
-    private boolean dfs(int row, int col, int start,char[][] board, String word) {
+
+    private boolean dfs(int row, int col, int start, char[][] board, String word) {
 
         int n = board.length;
         int m = board[0].length;
@@ -32,16 +32,11 @@ class Solution {
         char temp = board[row][col];
         board[row][col] = '*';
 
-        int[] dx = { -1, 0, 1, 0 };
-        int[] dy = { 0, 1, 0, -1 };
-
-        for (int i = 0; i < 4; i++) {
-            int newRow = row + dx[i];
-            int newCol = col + dy[i];
-
-            if (dfs(newRow, newCol, start + 1, board, word)) {
-                return true;
-            }
+        if (dfs(row - 1, col, start + 1, board, word) ||
+                dfs(row, col + 1, start + 1, board, word) ||
+                dfs(row + 1, col, start + 1, board, word) ||
+                dfs(row, col - 1, start + 1, board, word)) {
+            return true;
         }
         board[row][col] = temp;
         return false;
