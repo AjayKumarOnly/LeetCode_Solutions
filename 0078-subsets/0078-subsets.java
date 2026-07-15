af18a09ee704a4a742bbe855c6415e8e4d63c5ea
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-07-15 18:10:46 +0530

    Time: 0 ms (100%), Space: 44.3 MB (20.81%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -5,12 +5,14 @@ class Solution {
         backTracking(res,nums,0,temp);
         return res;
     }
-    private void backTracking(List<List<Integer>> res,int[] nums,int pos ,List<Integer> temp){
+    private void backTracking(List<List<Integer>> res ,int[] nums,int pos, List<Integer> temp){
         res.add(new ArrayList<>(temp));
         for(int i=pos;i<nums.length;i++){
             temp.add(nums[i]);
-             backTracking(res,nums,i+1,temp);
+            backTracking(res,nums,i+1,temp);
             temp.remove(temp.size()-1);
         }
     }
 }
+// T.C => O(2 ^ n * n);
+// S.C => O(n) + call stack
