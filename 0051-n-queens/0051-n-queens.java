4da413a8573ac72b95c7d65ff14d0e474e62a931
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-07-23 18:30:16 +0530

    Time: 2 ms (82.4%), Space: 46.5 MB (65.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -3,42 +3,43 @@ class Solution {
         List<List<String>> res = new ArrayList<>();
         char[][] board = new char[n][n];
 
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
             Arrays.fill(board[i],'.');
         }
-        backTrack(0,board,res,n);
+
+        backTrack(0,res,board,n);
         return res;
     }
-    private void backTrack(int col , char[][] board,List<List<String>> res,int n){
+    private void backTrack(int col , List<List<String>> res , char[][] board,int n){
         if(col == n){
             res.add(construct(board));
             return ;
         }
-        for(int row = 0; row < n;row++){
+        for(int row = 0; row < n ; row++){
             if(isValid(board,row,col,n)){
                 board[row][col] = 'Q';
-                backTrack(col+1,board,res,n);
+                backTrack(col+1,res,board,n);
                 board[row][col] = '.';
             }
         }
     }
-    private boolean isValid(char[][] board,int row,int col,int n){
-        for(int i=0;i<col;i++){
-            if(board[row][i] == 'Q') return false;
-        } 
-        for(int i = 1; row - i >= 0 && col - i >= 0; i++){
-            if(board[row-i][col - i] == 'Q') return false;
+    private List<String> construct(char[][] board){
+        List<String> temp = new ArrayList<>();
+        for(char[] row : board){
+            temp.add(new String(row));
         }
-        for(int i = 1; row + i < n && col - i >= 0; i++){
-            if(board[row+i][col - i] == 'Q') return false;
+        return temp;
+    }
+    private boolean isValid(char[][] board , int row, int col , int n){
+        for(int i = 0;  i < col;i++){
+            if(board[row][i] == 'Q') return false;
+        }
+        for(int i = 1 ; row - i >= 0 && col - i >= 0; i++){
+            if(board[row-i][col-i] == 'Q') return false;
+        }
+        for(int i = 1 ; row + i < n && col - i >= 0; i++){
+            if(board[row+i][col-i] == 'Q') return false;
         }
         return true;
-    }
-    private List<String> construct(char[][] board){
-        List<String> res = new ArrayList<>();
-        for(char[] row : board){
-            res.add(new String(row));
-        }
-        return res;
     }
 }
