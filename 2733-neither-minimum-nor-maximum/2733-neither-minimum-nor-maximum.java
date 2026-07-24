e12069aaab41bdc9138e41d165d90cc2ced65543
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-07-24 12:12:44 +0530

    Time: 12 ms (47.97%), Space: 47.5 MB (15.1%) - LeetHub

---
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if(nums.length < 3) return -1;
+        Arrays.sort(nums);
+        
+        return nums[nums.length/2];
+    }
+}
