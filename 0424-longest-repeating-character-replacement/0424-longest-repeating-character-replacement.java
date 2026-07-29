7393f84deb6eb0f078bf1cd07eb1fd8a6bc15e80
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-07-29 18:24:14 +0530

    Time: 36 ms (14.36%), Space: 47 MB (24.47%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,26 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        int left = 0  , maxFreq = 0 , maxWindowLength = 0;
         Map<Character,Integer> mpp = new HashMap<>();
-        int left = 0 , maxFreq = 0 , maxWindow = 0;
-
         for(int right = 0; right < s.length(); right++){
             char ch = s.charAt(right);
-
             mpp.put(ch,mpp.getOrDefault(ch,0)+1);
 
-            maxFreq = Math.max(maxFreq , mpp.get(ch));
-
-            int windowLength = right - left + 1;
-
-            int characterNeedsToBeChanged = windowLength - maxFreq;
-
-            if(characterNeedsToBeChanged > k){
-                char leftChar = s.charAt(left);
-                mpp.put(leftChar,mpp.get(leftChar)-1);
+            maxFreq = Math.max(mpp.get(ch),maxFreq);
+            
+            int charNeedToChange = (right-left+1) - maxFreq;
+            if(charNeedToChange > k){
+                char l = s.charAt(left);
+                mpp.put(l,mpp.get(l)-1);
                 left++;
             }
-            maxWindow = Math.max(maxWindow , right-left+1);
+            maxWindowLength = Math.max(maxWindowLength,(right-left+1));
         }
-        return maxWindow;
+        return maxWindowLength;
     }
 }
