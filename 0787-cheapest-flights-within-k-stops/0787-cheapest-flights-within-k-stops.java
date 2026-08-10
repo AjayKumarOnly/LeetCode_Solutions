4ad1fff3593a5d5d5d270996e19699af0b66459d
Ajay Kumar S <ajaykumar.msaravana@gmail.com>
2026-08-10 08:57:04 +0530

    Time: 7 ms (40.58%), Space: 46.6 MB (55.58%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,27 +1,36 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> adj = new HashMap<>();
 
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
+        for (int[] i : flights)
+            adj.computeIfAbsent(i[0], value -> new ArrayList<>()).add(new int[] { i[1], i[2] });
 
-        for (int i = 0; i <= k; i++) {
-            int[] temp = dist.clone();
-            for (int[] flight : flights) {
+        int[] stops = new int[n];
+        Arrays.fill(stops, Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        // {dist_from_src_node, node, number_of_stops_from_src_node}
+        pq.offer(new int[] { 0, src, 0 });
 
-                int from = flight[0];
-                int to = flight[1];
-                int price = flight[2];
+        while (!pq.isEmpty()) {
+            int[] temp = pq.poll();
+            int dist = temp[0];
+            int node = temp[1];
+            int steps = temp[2];
 
-                if (dist[from] != Integer.MAX_VALUE) {
-                    temp[to] = Math.min(
-                        temp[to],
-                        dist[from] + price
-                    );
-                }
+            // We have already encountered a path with a lower cost and fewer stops,
+            // or the number of stops exceeds the limit.
+            if (steps >= stops[node] || steps > k + 1)
+                continue;
+                
+            stops[node] = steps;
+            if (node == dst)
+                return dist;
+            if (!adj.containsKey(node))
+                continue;
+            for (int[] a : adj.get(node)) {
+                pq.offer(new int[] { dist + a[1], a[0], steps + 1 });
             }
-            dist = temp;
         }
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        return -1;
     }
 }
